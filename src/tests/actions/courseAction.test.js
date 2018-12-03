82b00533a56f07d7b13e155aa080f63a4479fcbd
llabake <lemboyelabake@gmail.com>
2018-12-03 16:28:45 +0100

    write test for course actions and author actions

---
+++ src/tests/actions/courseAction.test.js
@@ -0,0 +1,79 @@
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import expect from 'expect';
+
+import * as types from '../../actions/actionTypes';
+import * as actions from  '../../actions/courseActions';
+import { courses } from "../../api/mockCourseApi";
+import courseMock from '../mocks/courseMock';
+import authorMock from "../mocks/authorMock";
+import {authors} from "../../api/mockAuthorApi";
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+describe('Course Action test', () => {
+  it('should load all course', () => {
+    const expectedAction = [
+      { type: types.BEGIN_AJAX_CALL },
+      {
+        type: types.LOAD_COURSES_SUCCESS,
+        courses,
+      }];
+    const store = mockStore({});
+    return store.dispatch(actions.loadCourses()).then(() => {
+      expect(store.getActions()).toEqual(expectedAction);
+    })
+  });
+  it('Updates a course detail', () => {
+    const expectedAction = [
+      { type: types.BEGIN_AJAX_CALL },
+      {
+        type: types.UPDATE_COURSE_SUCCESS,
+        course: courseMock.updatedCourse,
+      }
+    ];
+    const store = mockStore({
+      courses: [{
+        id: "career-reboot-for-developer-mind",
+        title: "Becoming an Outlier: Reprogramming the Developer Mind Awesome",
+        watchHref: "http://www.pluralsight.com/courses/career-reboot-for-developer-mind",
+        authorId: "cory-house",
+        length: "2:30",
+        category: "Technology",
+      }]
+    });
+    return store.dispatch(actions.saveCourse(courseMock.updatedCourse))
+      .then(() => {
+        expect(store.getActions()).toEqual(expectedAction);
+      })
+  });
+  it('creates a course successfully', () => {
+    const expectedAction = [
+      { type: types.BEGIN_AJAX_CALL },
+      {
+        type: types.CREATE_COURSE_SUCCESS,
+        course: courseMock.createdCourseWithId,
+      }
+    ];
+    const store = mockStore({});
+    return store.dispatch(actions.saveCourse(courseMock.createdCourse))
+      .then(() => {
+        console.log(store.getActions(), 'plplfpfl')
+        expect(store.getActions()).toEqual(expectedAction);
+      })
+  });
+  it('Deletes a course successfully', () => {
+    const expectedAction = [
+      {
+        type: types.DELETE_COURSE_SUCCESS,
+        courseId: courses[2].id,
+      }
+    ];
+    const store = mockStore({});
+    return store.dispatch(actions.deleteCourse(courses[2].id))
+      .then(() => {
+        expect(store.getActions()).toEqual(expectedAction);
+      })
+  });
+});
---
+++ src/tests/mocks/authorMock.js
@@ -0,0 +1,28 @@
+export default {
+  authors: [
+    {
+      id: 'mock-author-one',
+      firstName: 'author_one',
+      lastName: 'mock-author'
+    },
+    {
+      id: 'mock-author-two',
+      firstName: 'author_two',
+      lastName: 'mock-author-two'
+    }
+  ],
+  updatedAuthor: {
+      id: 'cory-house',
+      firstName: 'author_one',
+      lastName: 'mock-author'
+  },
+  createdAuthor: {
+    firstName: 'Sardaunan',
+    lastName: 'Happygirl'
+  },
+  createdAuthorWithId: {
+    id: 'sardaunan-happygirl',
+    firstName: 'Sardaunan',
+    lastName: 'Happygirl'
+  }
+}
